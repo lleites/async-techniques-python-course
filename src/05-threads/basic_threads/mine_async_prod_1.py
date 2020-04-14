d66e0d5fbe20348c7512459c72d279692e0b11de
leandro.leites <leandro.leites@flixbus.com>
2020-04-14 22:24:54 +0200

    Added mine  basic_threads 1

---
+++ src/05-threads/basic_threads/mine_async_prod_1.py
@@ -0,0 +1,66 @@
+import datetime
+import colorama
+import random
+import time
+
+import threading
+
+
+def main():
+    t0 = datetime.datetime.now()
+    print(colorama.Fore.WHITE + "App started.", flush=True)
+    data = []
+
+    threads = [
+        threading.Thread(target=generate_data, args=(20, data)),
+        threading.Thread(target=generate_data, args=(20, data)),
+        threading.Thread(target=process_data, args=(40, data)),
+    ]
+
+    [thread.start() for thread in threads]
+    [thread.join() for thread in threads]
+
+    dt = datetime.datetime.now() - t0
+    print(
+        colorama.Fore.WHITE
+        + "App exiting, total time: {:,.2f} sec.".format(dt.total_seconds()),
+        flush=True,
+    )
+
+
+def generate_data(num: int, data: list):
+    for idx in range(1, num + 1):
+        item = idx * idx
+        data.append((item, datetime.datetime.now()))
+
+        print(colorama.Fore.YELLOW + f" -- generated item {idx}", flush=True)
+        time.sleep(random.random() + 0.5)
+
+
+def process_data(num: int, data: list):
+    processed = 0
+    while processed < num:
+        item = None
+        if data:
+            item = data.pop(0)
+        if not item:
+            time.sleep(0.01)
+            continue
+
+        processed += 1
+        value = item[0]
+        t = item[1]
+        dt = datetime.datetime.now() - t
+
+        print(
+            colorama.Fore.CYAN
+            + " +++ Processed value {} after {:,.2f} sec.".format(
+                value, dt.total_seconds()
+            ),
+            flush=True,
+        )
+        time.sleep(0.5)
+
+
+if __name__ == "__main__":
+    main()
